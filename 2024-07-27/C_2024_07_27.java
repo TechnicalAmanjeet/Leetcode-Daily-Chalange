4d70197a9265275304fb0536be430c13a18aa57d
Amanjeet Kumar <amanjeet.kumar@cogoport.com>
2024-07-22 16:05:22 +0530

    using person class whose property is name and height and sorted them on the basis of height

--- 2024-07-27/C_2024_07_27.java
+++ 2024-07-27/C_2024_07_27.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 
 import static java.util.Collections.sort;
@@ -22,21 +23,52 @@ public class C_2024_07_27 {
 
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
-        int[] cloneHeights = heights.clone();
-        Arrays.sort(heights);
+        class Person {
+            String name;
+            int height;
 
-        HashMap<Integer, Integer> heightNameMappings = new HashMap<Integer, Integer>();
+            Person(
+                    String name,
+                    int height
+            ) {
+                this.name = name;
+                this.height = height;
+            }
+
+            public int getHeight() {
+                return height;
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            @Override
+            public String toString() {
+                return this.name + " (" + this.height + ")";
+            }
+
+            public static int compareHeight(Person p1, Person p2) {
+                return p1.height - p2.height;
+            }
+        }
+
+        Person[] peoples = new Person[names.length];
 
         for (int i = 0; i < names.length; i++) {
-            heightNameMappings.put(cloneHeights[i], i);
+            peoples[i] = new Person(names[i], heights[i]);
+        }
+        Arrays.sort(peoples, new Comparator<Person>() {
+            @Override
+            public int compare(Person p1, Person p2) {
+                return Person.compareHeight(p1, p2);
+            }
+        });
+
+        for (int i = 0; i < peoples.length; i++) {
+            names[i] = peoples[peoples.length - i - 1].getName();
         }
 
-        String[] result = names.clone();
-
-        for (int i = names.length-1; i >= 0; i--) {
-            result[heights.length - i -1] = names[heightNameMappings.get(heights[i])];
-        }
-
-        return result;
+        return names;
     }
 }
