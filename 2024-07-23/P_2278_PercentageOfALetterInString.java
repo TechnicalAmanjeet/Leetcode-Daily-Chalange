f5115785f5ccaddd99086c570c815b655f1fc3c2
Amanjeet Kumar <amanjeet.kumar@cogoport.com>
2024-07-23 12:39:05 +0530

    done

---
+++ 2024-07-23/P_2278_PercentageOfALetterInString.java
@@ -0,0 +1,23 @@
+public class P_2278_PercentageOfALetterInString {
+    String s = "foobar";
+    Char letter = 'o';
+
+    SolutionP_2278 solution = new SolutionP_2278();
+
+    System.out.println(solution.percentageLetter(
+            s = s,
+            letter = letter
+            ));
+}
+
+class SolutionP_2278 {
+    public int percentageLetter(String s, char letter) {
+        int count = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == letter) {++count;}
+        }
+        
+        return count/s.length();
+    }
+}
