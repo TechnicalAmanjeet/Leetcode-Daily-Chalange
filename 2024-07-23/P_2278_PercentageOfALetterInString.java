7e0117a0ee52c39c39cd32cd6761fec36d4606d0
Amanjeet Kumar <amanjeet.kumar@cogoport.com>
2024-07-23 12:44:05 +0530

    100% solution

--- 2024-07-23/P_2278_PercentageOfALetterInString.java
+++ 2024-07-23/P_2278_PercentageOfALetterInString.java
@@ -12,12 +12,14 @@ public class P_2278_PercentageOfALetterInString {
 
 class SolutionP_2278 {
     public int percentageLetter(String s, char letter) {
-        int count = 0;
-        
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == letter) {++count;}
+        public int percentageLetter(String s, char letter) {
+            int count = 0;
+
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == letter) {++count;}
+            }
+
+            return (count*100)/s.length();
         }
-        
-        return count/s.length();
     }
 }
